9ba3cdc9bc2068e96dd7a4803415e954cdbbe01e
Sydney Ward <sydneyward@Sydneys-MBP-2.attlocal.net>
2022-10-02 20:06:58 -0500

    feat: add hello world program

---
+++ hello-world.ino
@@ -0,0 +1,22 @@
+// Hello World program
+// Send 'R' and it flashes a light and writes Hello World!
+
+int val;
+int ledpin = 13;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(ledpin, OUTPUT);
+}
+
+void loop() {
+  val = Serial.read();
+  if(val == 'R') {
+    digitalWrite(ledpin,HIGH);
+    delay(500);
+    digitalWrite(ledpin, LOW);
+    delay(500);
+    Serial.println("Hello World!");
+  }
+}
+
